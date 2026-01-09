a175b7451b965c6b0cc7605f94488fefb5bb53c6
Nagendra Mannem <67579881+Nagendra021091@users.noreply.github.com>
2026-01-09 16:05:26 +0530

    Add outputs for S3 bucket and Redshift role ARN

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,11 @@
+output "bucket_name" {
+  value = aws_s3_bucket.raw_bucket.bucket
+}
+
+output "redshift_role_arn" {
+  value = aws_iam_role.redshift_role.arn
+}
+
 output "redshift_workgroup" {
   value = aws_redshiftserverless_workgroup.wg.workgroup_name
 }
