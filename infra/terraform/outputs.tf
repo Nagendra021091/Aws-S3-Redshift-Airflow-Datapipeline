2f6f7f2d35fcf1b7ef9e65e2cb67bd8f91556792
Nagendra Mannem <67579881+Nagendra021091@users.noreply.github.com>
2026-01-09 16:03:47 +0530

    Add Redshift workgroup and namespace outputs
    Added outputs for Redshift workgroup and namespace.

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "bucket_name" {
-  value = aws_s3_bucket.raw_bucket.bucket
+output "redshift_workgroup" {
+  value = aws_redshiftserverless_workgroup.wg.workgroup_name
 }
 
-output "redshift_role_arn" {
-  value = aws_iam_role.redshift_role.arn
+output "redshift_namespace" {
+  value = aws_redshiftserverless_namespace.ns.namespace_name
 }
