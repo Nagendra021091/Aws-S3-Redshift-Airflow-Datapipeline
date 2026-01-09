11d20604e3dc4eb92bf52ad7d81e7e57716674c5
Nagendra Mannem <67579881+Nagendra021091@users.noreply.github.com>
2026-01-09 21:57:36 +0530

    Update DAG loading test to specify DAG folder

--- airflow/tests/test_dag_structure.py
+++ airflow/tests/test_dag_structure.py
@@ -1,5 +1,8 @@
 from airflow.models import DagBag
+import os
 
 def test_dag_loaded():
-    dagbag = DagBag()
+    dags_path = os.path.join(os.getcwd(), "airflow", "dags")
+    dagbag = DagBag(dag_folder=dags_path, include_examples=False)
+
     assert "s3_to_redshift_pipeline" in dagbag.dags
